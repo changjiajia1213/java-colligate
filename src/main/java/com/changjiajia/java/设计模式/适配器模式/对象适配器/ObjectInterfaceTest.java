37236832c5df8a1b02ff007aa3c6dff343414425
changjiajia <2245741634@qq.com>
2020-06-29 21:13:11 +0800

    ThreadLocal测试

---
+++ src/main/java/com/changjiajia/java/设计模式/适配器模式/对象适配器/ObjectInterfaceTest.java
@@ -0,0 +1,4 @@
+package com.changjiajia.java.设计模式.适配器模式.对象适配器;
+
+public interface ObjectInterfaceTest {
+}
---
+++ src/main/java/com/changjiajia/java/设计模式/适配器模式/对象适配器/TargetObject.java
@@ -0,0 +1,10 @@
+package com.changjiajia.java.设计模式.适配器模式.对象适配器;
+
+/**
+ * @program: java-colligate
+ * 作者: ChangJiaJia
+ * 日期: 2020-06-29 11:29
+ * 描述:
+ **/
+public class TargetObject {
+}
